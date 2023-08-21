801d59a24822cf3a7f2355f29f41fe246333e39b
JoelD-418 <joeldisidi8@gmail.com>
2023-08-21 19:02:31 +0000

    mixed-message-fait-avec-parseInt

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const message = ["Dormir","manger", "voyager"];
-let random = Math.floor(Math.random()*(message.length));
-let x = message[random];
-console.log(x);
+const message = ["Dormir", "manger", "voyager"];
+// const message = [1, "manger", "voyager"];
+let random = parseInt((Math.random() * (message.length)));
+// let afficheMessage =;
+console.log( message[random]);
